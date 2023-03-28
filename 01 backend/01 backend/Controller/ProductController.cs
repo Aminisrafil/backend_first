7cea36e2abd5d8027492a664836add31906b259b
Amin <aminisrafilzadeh24@gmail.com>
2023-03-29 01:11:40 +0400

    homeword added

---
+++ 01 backend/01 backend/Controller/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01_backend
+{
+    public class ProductController : Controller
+    {
+    
+        public ContentResult Detail(int id)
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "Product id:" + id;
+            return result;
+        }
+    }
+}
